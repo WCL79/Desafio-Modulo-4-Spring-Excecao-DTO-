7c5147eeb3e56809896a6e99918168bf6ec5f63e
Weslley Lima <weslley.lima@zup.com.br>
2021-03-10 14:57:57 -0300

    Criar pacote controller com classe ClienteController

---
+++ src/main/java/br/com/zup/desafiocatalisa100321/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package br.com.zup.desafiocatalisa100321.controller;
+/**
+ * Classe controller responsavel pela interação com a Classe Cliente da model ao services ClienteService
+ * @author Weslley.candido
+ * @version 0.01
+ */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequestMapping("/cliente")
+public class ClienteController {
+
+
+}
